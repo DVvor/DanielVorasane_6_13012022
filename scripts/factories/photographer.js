ce0e7f138c19b82a00a4aa57425519d6b01444e9
DVvor <85024600+DVvor@users.noreply.github.com>
2022-01-26 16:40:32 +0100

    homepage visible with photographers

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,44 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, city, country, tagline, price } = data;
     const picture = `assets/photographers/${portrait}`;
+
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const localisation = document.createElement ('h3');
+        localisation.textContent = `${city} , ${country}`;
+        
+        const slogan = document.createElement ('p');
+        slogan.classList.add("slogan");
+        slogan.textContent = tagline;
+
+        const tjm = document.createElement ('p');
+        tjm.classList.add("tjm");
+        tjm.textContent = `${price}€/jour`;
+
+        const link = document.createElement ('a');
+        link.href = "photographer.html?='${name}'";
+        link.title = "photographer page";
+        link.classList.add("link");
+        
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(localisation);
+        article.appendChild(slogan);
+        article.appendChild(tjm);
+        article.appendChild(link);
+
+        // article.appendChild();
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    // return { name, picture, getUserCardDOM }
+    return { getUserCardDOM }
 }
+
+
